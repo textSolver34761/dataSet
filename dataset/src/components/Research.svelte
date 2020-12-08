7b5e7d4950d99967ba58eb59bc2d051b5c9d5ef0
Abde <abdenour.bensouna@rocketstud.io>
2020-12-08 16:49:33 +0100

    laste modif

--- dataset/src/components/Research.svelte
+++ dataset/src/components/Research.svelte
@@ -1,7 +1,56 @@
 <script>
+  import { goto } from "@sapper/app";
+
+  let search = "";
+  let keyCode;
+
+  function redirect() {
+    if (search !== "") {
+      search = { search };
+    }
+  }
+  function handleKeydown(event) {
+    if (search !== "") {
+      keyCode = event.keyCode;
+      if (keyCode === 13) {
+        redirect();
+      }
+    }
+  }
 </script>
 
 <style>
+  .search {
+    display: block;
+    float: right;
+    border: none;
+    outline: none;
+    border-radius: 0.5rem;
+    margin-right: 1rem;
+  }
+  button,
+  input[type="text"] {
+    float: right;
+    border: none;
+    outline: none;
+    padding: 1em 0.5em;
+    margin-top: 0.1rem;
+    font-size: 1rem;
+    font-weight: 300;
+  }
+
+  input {
+    border-radius: 0.5rem 0 0 0.5rem;
+  }
+  button {
+    cursor: pointer;
+    border-radius: 0 0.5rem 0.5rem 0;
+  }
 </style>
 
-<form action="" method="post" />
+<svelte:window on:keydown={handleKeydown} />
+
+<div class="search">
+  <button on:click={redirect}>search</button>
+  <input type="text" bind:value={search} placeholder="Recherche..." />
+</div>
