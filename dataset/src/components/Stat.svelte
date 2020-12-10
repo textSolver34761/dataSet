d05aac5d6d2bc116c543acd66ef56c1657d8eb82
Abde <abdenour.bensouna@rocketstud.io>
2020-12-10 12:40:10 +0100

    refacto and graph in tha place

--- dataset/src/components/Stat.svelte
+++ dataset/src/components/Stat.svelte
@@ -1,97 +1,110 @@
 <script>
-    import Modele from "../components/Modele.svelte";
-    import Etat from "../components/Etat.svelte";
-    import Pointure from "../components/PointurePlus.svelte";
-  </script>
-  
-  <style>
-      
-  </style>
+  import GraphData from "./GraphData.svelte";
 
-<!-- Les pointures les plus vendus sont : {
-  "10.0": 11093,
-  "9.0": 9706,
-  "11.0": 9251,
-  "10.5": 8784,
-  "9.5": 8685,
-  "12.0": 7297,
-  "8.0": 5363,
-  "8.5": 5300,
-  "13.0": 4602,
-  "11.5": 4502
-}
+  let data = {
+    message: [
+      "Taille la plus acheter",
+      "Etat les plus acheteurs",
+      "Model les plus acheter",
+      "Taille les mois acheter",
+      "Etat les moins acheteurs",
+      "Model les moins acheter",
+    ],
 
+    shoeMostSold: [
+      { x: "8.0", y: 5363 },
+      { x: "8.5", y: 5300 },
+      { x: "9.0", y: 9706 },
+      { x: "9.5", y: 8685 },
+      { x: "10.0", y: 11093 },
+      { x: "10.5", y: 8784 },
+      { x: "11.0", y: 9251 },
+      { x: "11.5", y: 4502 },
+      { x: "12.0", y: 7297 },
+      { x: "13.0", y: 4602 },
+    ],
 
-Les états qui achète le plus sont : {
-  "California": 19349,
-  "New York": 16525,
-  "Oregon": 7681,
-  "Florida": 6376,
-  "Texas": 5876,
-  "New Jersey": 4720,
-  "Illinois": 3782,
-  "Pennsylvania": 3109,
-  "Massachusetts": 2903,
-  "Michigan": 2762
-}
+    statsMostB: [
+      { x: "California", y: 19349 },
+      { x: "NewYork", y: 16525 },
+      { x: "Oregon", y: 7681 },
+      { x: "Florida", y: 6376 },
+      { x: "Texas", anount: 5876 },
+      { x: "NewJersey", y: 4720 },
+      { x: "Illinois", y: 3782 },
+      { x: "Pennsylvania", y: 3109 },
+      { x: "Massachusetts", y: 2903 },
+      { x: "Michigan", y: 2762 },
+    ],
 
+    modelMostSold: [
+      { x: "Adidas-Yeezy-Boost-350-V2-Butter", y: 11423 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Beluga-2pt0", y: 10395 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Zebra", y: 10110 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Blue-Tint", y: 9297 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Cream-White", y: 9097 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Sesame", y: 5553 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Static", y: 4983 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Semi-Frozen-Yellow", y: 4854 },
+      {
+        x: "Air-Jordan-1-Retro-High-Off-White-University-Blue",
+        y: 4635,
+      },
+      { x: "Adidas-Yeezy-Boost-350-V2-Static-Reflective", y: 3184 },
+    ],
 
-Les modèles les plus vendus sont : {
-  "adidas-Yeezy-Boost-350-V2-Butter": 11423,
-  "Adidas-Yeezy-Boost-350-V2-Beluga-2pt0": 10395,
-  "Adidas-Yeezy-Boost-350-V2-Zebra": 10110,
-  "Adidas-Yeezy-Boost-350-V2-Blue-Tint": 9297,
-  "Adidas-Yeezy-Boost-350-V2-Cream-White": 9097,
-  "Adidas-Yeezy-Boost-350-V2-Sesame": 5553,
-  "adidas-Yeezy-Boost-350-V2-Static": 4983,
-  "Adidas-Yeezy-Boost-350-V2-Semi-Frozen-Yellow": 4854,
-  "Air-Jordan-1-Retro-High-Off-White-University-Blue": 4635,
-  "adidas-Yeezy-Boost-350-V2-Static-Reflective": 3184
-}
+    statsLessB: [
+      { x: "WestVirginia", y: 166 },
+      { x: "Mississippi", y: 163 },
+      { x: "Maine", y: 132 },
+      { x: "Idaho", y: 107 },
+      { x: "Vermont", y: 84 },
+      { x: "NorthDakota", y: 64 },
+      { x: "Alaska", y: 61 },
+      { x: "SouthDakota", y: 55 },
+      { x: "Montana", y: 49 },
+      { x: "Wyoming", y: 40 },
+    ],
 
-Les états qui achète le moins sont : {
-  "West Virginia": 166,
-  "Mississippi": 163,
-  "Maine": 132,
-  "Idaho": 107,
-  "Vermont": 84,
-  "North Dakota": 64,
-  "Alaska": 61,
-  "South Dakota": 55,
-  "Montana": 49,
-  "Wyoming": 40
-}
+    sizeLessSold: [
+      { x: "3.5", y: 4 },
+      { x: "4.5", y: 1304 },
+      { x: "6.5", y: 2219 },
+      { x: "12.5", y: 628 },
+      { x: "13.5", y: 147 },
+      { x: "14.0", y: 1791 },
+      { x: "14.5", y: 84 },
+      { x: "15.0", y: 130 },
+      { x: "16.0", y: 79 },
+      { x: "17.0", y: 4 },
+    ],
 
-Les pointures les moins vendus sont : {
-  "6.5": 2219,
-  "14.0": 1791,
-  "4.5": 1304,
-  "12.5": 628,
-  "13.5": 147,
-  "15.0": 130,
-  "14.5": 84,
-  "16.0": 79,
-  "17.0": 4,
-  "3.5": 4
-}
+    modelLessSold: [
+      { x: "Nike-Blazer-Mid-Off-White-Wolf-Grey", y: 288 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Core-Black-Copper", y: 225 },
+      { x: "Nike-Air-Max-97-Off-White-Elemental-Rose-Queen", y: 216 },
+      { x: "Adidas-Yeezy-Boost-350-V2-Core-Black-Green", y: 196 },
+      { x: "Adidas-Yeezy-Boost-350-Low-Pirate-Black-2016", y: 122 },
+      { x: "Adidas-Yeezy-Boost-350-Low-Moonrock", y: 100 },
+      { x: "Adidas-Yeezy-Boost-350-Low-Pirate-Black-2015", y: 95 },
+      { x: "Adidas-Yeezy-Boost-350-Low-Oxford-Tan", y: 70 },
+      { x: "Adidas-Yeezy-Boost-350-Low-Turtledove", y: 68 },
+      { x: "Nike-Air-Force-1-Low-Virgil-Abloh-Off-White-AF100", y: 31 },
+    ],
+  };
+</script>
 
-Les modèles les moins vendus sont : {
-  "Nike-Blazer-Mid-Off-White-Wolf-Grey": 288,
-  "Adidas-Yeezy-Boost-350-V2-Core-Black-Copper": 225,
-  "Nike-Air-Max-97-Off-White-Elemental-Rose-Queen": 216,
-  "Adidas-Yeezy-Boost-350-V2-Core-Black-Green": 196,
-  "Adidas-Yeezy-Boost-350-Low-Pirate-Black-2016": 122,
-  "Adidas-Yeezy-Boost-350-Low-Moonrock": 100,
-  "Adidas-Yeezy-Boost-350-Low-Pirate-Black-2015": 95,
-  "Adidas-Yeezy-Boost-350-Low-Oxford-Tan": 70,
-  "Adidas-Yeezy-Boost-350-Low-Turtledove": 68,
-  "Nike-Air-Force-1-Low-Virgil-Abloh-Off-White-AF100": 31
-} -->
+<style>
+</style>
 
-  <Modele />
-  <br>
-  <br>
-  <Etat />
-  <br>
-  <br>
-  <Pointure />
+<GraphData msg={data.message[0]} data={data.shoeMostSold} />
+<br />
+<GraphData msg={data.message[1]} data={data.statsMostB} />
+<br />
+<GraphData msg={data.message[2]} data={data.modelMostSold} />
+<br />
+<GraphData msg={data.message[3]} data={data.statsLessB} />
+<br />
+<GraphData msg={data.message[4]} data={data.sizeLessSold} />
+<br />
+<GraphData msg={data.message[5]} data={data.modelLessSold} />
